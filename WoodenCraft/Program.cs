df6633644d4bad0a6f1e0a272df853f9ac8ae6e9
vin1992 <vincoder1992@gmail.com>
2025-04-21 15:11:36 -0400

    Assemble RadzenCarousel, a third party lib,  into the project

--- WoodenCraft/Program.cs
+++ WoodenCraft/Program.cs
@@ -1,10 +1,14 @@
 using WoodenCraft.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
